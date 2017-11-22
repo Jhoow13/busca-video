98a825edb2b60e6745e7740bf833b4cc4e7a833f
Joel Silva <joelcsilva@outlook.com>
2017-11-22 21:33:43 -0200

    Implementando paginacao

--- app/controllers/inicialController.js
+++ app/controllers/inicialController.js
@@ -5,14 +5,29 @@
                ['$scope','searchHttpServices',
         function($scope,searchHttpServices){
 
+            $scope.search_field = '';
+            $scope.searchResults = [];
+
             $scope.search = function(search_field){
                 searchHttpServices.defaultRequest(search_field).then(function(response){
-
+                    var pageInfo = response.data.pageInfo;
+                    $scope.nextPage = response.data.nextPageToken
+                    $scope.totalPages = Math.ceil(pageInfo.totalResults/10);
+                    $scope.currentPage = 1;
                     $scope.searchResults = response.data.items;
                 });
             }
 
+            $scope.getPage = function(pageToken, pageControl){
+                console.log(pageToken);
+                searchHttpServices.pageRequest($scope.search_field, pageToken).then(function(response){
+                    $scope.searchResults = response.data.items;
+                    $scope.nextPage = response.data.nextPageToken;
+                    $scope.prevPage = response.data.prevPageToken;
 
+                    pageControl ? $scope.currentPage = $scope.currentPage + 1 : $scope.currentPage = $scope.currentPage - 1;
+
+                });
+            }
         }]);
-
 })();
--- app/services/searchHttpService.js
+++ app/services/searchHttpService.js
@@ -5,6 +5,7 @@
 
             var KEY = 'AIzaSyAZ7g-fZdfwaVZ7gsm0wjQY7vxcMgK999U';
             var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search';
+            var partParams = 'id,snippet';
 
             var _defaultRequest = function(search_field){
                 return $http({
@@ -13,13 +14,29 @@
                     params:{
                         key: KEY,
                         q: search_field,
-                        part: 'id,snippet'
+                        part: partParams,
+                        maxResults: 10
                     }
                 });
             };
 
+            var _pageRequest = function(search_field, pageToken){
+                return $http({
+                    method: 'GET',
+                    url: youtubeUrl,
+                    params:{
+                        key: KEY,
+                        q:search_field,
+                        part: partParams,
+                        pageToken: pageToken,
+                        maxResults: 10
+                    }
+                })
+            }
+
             return {
-                defaultRequest: _defaultRequest
+                defaultRequest: _defaultRequest,
+                pageRequest: _pageRequest
             };
 
         }]);
