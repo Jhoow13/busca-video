334362e7d1f5b71185dc9f59c2ca045b82bcd8f1
Joel Silva <joelcsilva@outlook.com>
2017-11-22 17:50:39 -0200

    Chamada http para pesquisa

--- app/services/searchHttpService.js
+++ app/services/searchHttpService.js
@@ -2,15 +2,24 @@
     'use strict';
     angular.module('buscaVideo')
         .factory('searchHttpServices', ['$http', function ($http){
-            var _defaultRequest = function(){
+
+            var KEY = 'AIzaSyAZ7g-fZdfwaVZ7gsm0wjQY7vxcMgK999U';
+            var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+            var _defaultRequest = function(search_field){
                 return $http({
                     method: 'GET',
-                    url: ''
+                    url: youtubeUrl,
+                    params:{
+                        key: KEY,
+                        q: search_field,
+                        part: 'id,snippet'
+                    }
                 });
             };
 
             return {
-                _defaultRequest: defaultRequest
+                defaultRequest: _defaultRequest
             };
 
         }]);
