25fb3da62a2f133137aab25dd1436129e9dcd7b4
Joel Silva <joelcsilva@outlook.com>
2017-11-22 17:51:39 -0200

    Função de pesquisa

--- app/controllers/inicialController.js
+++ app/controllers/inicialController.js
@@ -2,9 +2,15 @@
     'use strict';
 
     angular.module('buscaVideo').controller('inicialController',
-               ['$scope', '$location',
-        function($scope,$location){
+               ['$scope','searchHttpServices',
+        function($scope,searchHttpServices){
 
+            $scope.search = function(search_field){
+                searchHttpServices.defaultRequest(search_field).then(function(response){
+
+                    $scope.searchResults = response.data.items;
+                });
+            }
 
 
         }]);
