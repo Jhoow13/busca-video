d101400a7880a10d8f86da66808f07b3815c4bb8
Joel Silva <joelcsilva@outlook.com>
2017-11-23 04:12:04 -0200

    Utilizando models

--- app/controllers/inicialController.js
+++ app/controllers/inicialController.js
@@ -2,11 +2,10 @@
     'use strict';
 
     angular.module('buscaVideo').controller('inicialController',
-               ['$scope','searchHttpServices',
-        function($scope,searchHttpServices){
+               ['$scope','applicationModel','searchHttpServices',
+        function($scope,applicationModel,searchHttpServices){
 
             $scope.search_field = '';
-            $scope.searchResults = [];
 
             $scope.search = function(search_field){
                 searchHttpServices.defaultRequest(search_field).then(function(response){
@@ -14,25 +13,22 @@
                     $scope.nextPage = response.data.nextPageToken
                     $scope.totalPages = Math.ceil(pageInfo.totalResults/10);
                     $scope.currentPage = 1;
-                    $scope.searchResults = response.data.items;
+                    $scope.searchResults = applicationModel.searchResultModel(response.data.items);
                 });
             }
 
             $scope.getPage = function(pageToken, pageControl){
-                console.log(pageToken);
                 searchHttpServices.pageRequest($scope.search_field, pageToken).then(function(response){
-                    $scope.searchResults = response.data.items;
                     $scope.nextPage = response.data.nextPageToken;
                     $scope.prevPage = response.data.prevPageToken;
-
+                    $scope.searchResults = applicationModel.searchResultModel(response.data.items);
                     pageControl ? $scope.currentPage = $scope.currentPage + 1 : $scope.currentPage = $scope.currentPage - 1;
-
                 });
             }
 
             $scope.videoDetails = function(videoId){
-                searchHttpServices.videoDetailsRequest().then(function(response){
-
+                searchHttpServices.videoDetailsRequest(videoId).then(function(response){
+                    $scope.details = applicationModel.videoDetailsModel(response.data.items);
                 });
             }
 
