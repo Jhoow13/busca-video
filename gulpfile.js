30db0c0677b1683ee846cd13fcf300e4c73f8486
Joel Silva <joelcsilva@outlook.com>
2017-11-23 22:52:07 -0200

    Criando tarefas de build

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,20 @@ var jshint = require('gulp-jshint');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
 
 var appFiles = {
     htmlfiles:[
-        'index.html',
         './app/views/*.html',
         './app/**/*.html'
     ],
     cssFiles:[
         './app/assets/*.less'
+    ],
+    cssFilesProd:[
+        'node_modules/angular-material/angular-material.min.css'
     ],
     jsFiles: [
         './app/*.js',
@@ -20,13 +25,24 @@ var appFiles = {
         './app/services/**/*.js',
         './app/models/**/*.js',
         './app/controllers/*.js'
+    ],
+    jsFilesProd:[
+        'node_modules/angular/angular.min.js',
+        'node_modules/angular-route/angular-route.min.js',
+        'node_modules/angular-sanitize/angular-sanitize.min.js',
+        'node_modules/angular-animate/angular-animate.min.js',
+        'node_modules/angular-aria/angular-aria.min.js',
+        'node_modules/angular-messages/angular-messages.min.js',
+        'node_modules/angular-material/angular-material.min.css'
     ]
 };
 
 gulp.task('clean', function(){
-    return gulp.src('src/*')
+     gulp.src('src/**/*.*')
     .pipe(clean());
 });
+
+//** DEV TASKS **//
 
 gulp.task('jshint', function(){
     return gulp.src(appFiles.jsFiles)
@@ -47,6 +63,44 @@ gulp.task('less', function(){
     .pipe(gulp.dest('src/'));
 });
 
+//** BUILD TASKS **//
+
+gulp.task('htmlFiles-prod', function(){
+    return gulp.src(appFiles.htmlfiles)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('src/html/'))
+});
+
+gulp.task('cssDependences-prod', function(){
+    return gulp.src(appFiles.cssFilesProd)
+    .pipe(concat('assets.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('src/css/'));
+});
+
+gulp.task('cssFiles-prod', function(){
+    return gulp.src(appFiles.cssFiles)
+    .pipe(concat('style.css'))
+    .pipe(less())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('src/css/'));
+});
+
+gulp.task('jsDependences-prod', function(){
+    return gulp.src(appFiles.jsFilesProd)
+    .pipe(concat('dependences.js'))
+    .pipe(gulp.dest('src/js/'));
+});
+
+gulp.task('jsFiles-prod', function(){
+    return gulp.src(appFiles.jsFiles)
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('src/js/'));
+});
+
+//** BROWSER RELOAD TASKS **//
+
 gulp.task('reload', function(done){
     browserSync.reload();
     done();
@@ -65,4 +119,5 @@ gulp.task('serve', ['less', 'js'], function(){
 
 });
 
+gulp.task('build', ['clean', 'htmlFiles-prod', 'cssDependences-prod', 'cssFiles-prod', 'jsDependences-prod', 'jsFiles-prod'])
 gulp.task('default',['serve']);
